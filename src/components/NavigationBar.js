838ce5290fcbb7645c0c585f2c59fe489721d4eb
iamsuru <suryanshshrivastava6364@gmail.com>
2023-11-27 15:44:46 +0530

    0.1.1

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -22,6 +22,9 @@ function NavigationBar() {
                             <NavItem>
                                 <Link className='nav-nav-link mx-4' to='/form'>Form</Link>
                             </NavItem>
+                            <NavItem>
+                                <Link className='nav-nav-link mx-4' to='/upload'>Upload</Link>
+                            </NavItem>
                         </ul>
                     </div>
                 </div>
