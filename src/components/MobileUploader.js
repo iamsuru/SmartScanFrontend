cf0c9078737ec5313f27a793f8116bb9687e41d0
iamsuru <suryanshshrivastava6364@gmail.com>
2023-11-29 17:41:46 +0530

    qrcode

--- src/components/MobileUploader.js
+++ src/components/MobileUploader.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function MobileUploader() {
-    // const [resFromServer, setData] = useState('');
+    const [resFromServer, setData] = useState('');
 
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search)
@@ -25,9 +25,7 @@ function MobileUploader() {
                 body: JSON.stringify({ location })
             })
             console.warn(location);
-            const data = await response.json()
-            console.info(data.message)
-            // setData(data)
+            setData(response)
         }
         catch (error) {
             console.error('Error sending location:', error);
@@ -35,46 +33,49 @@ function MobileUploader() {
         }
     }
 
-    // if (resFromServer && resFromServer.message) {
-    //     toast.success(resFromServer.message);
-    // }
+    const [selectedFile, setSelectedFile] = useState(null)
 
-    // useEffect(() => {
-    //     const sendLocation = async () => {
-    //         const urlParams = new URLSearchParams(window.location.search);
-    //         const locationParameter = urlParams.get('location');
-    //         console.log('Location Parameter:', locationParameter);
+    const handleFileInput = (e) => {
+        setSelectedFile(e.target.files[0])
+    }
 
-    //         try {
-    //             const response = await fetch('https://smartscanbackend.up.railway.app/api/sendLocationString', {
-    //                 method: 'POST',
-    //                 headers: {
-    //                     'Content-Type': 'application/json',
-    //                 },
-    //                 body: JSON.stringify({ locationParameter }),
-    //             });
+    const uploadToServer = async (e) => {
+        e.preventDefault()
+        if (resFromServer.ok) {
+            try {
+                if (!selectedFile) {
+                    toast.error('Please select a file')
+                    return
+                }
 
-    //             const data = await response.json();
-    //             setData(data);
-    //         } catch (error) {
-    //             console.error('Error sending location:', error);
-    //             toast.error('Error sending location');
-    //         }
-    //     };
+                const formData = new FormData()
+                formData.append('document-to-upload', selectedFile)
+                const response = await fetch('https://smartscanbackend.up.railway.app/api/uploadFromMobile', {
+                    method: 'POST',
+                    body: formData
+                })
+                if (response.ok) {
+                    toast.success('File Uploaded')
+                }
+                else {
+                    const errorData = await response.json();
+                    toast.error(errorData.error || 'File upload failed');
+                }
+            }
+            catch (error) {
+                console.error('Error uploading file:', error);
+                toast.error('Error uploading file');
+            }
+        }
+    }
 
-    //     sendLocation();
-    // }, []);
-
-    // if (resFromServer && resFromServer.message) {
-    //     toast.success(resFromServer.message);
-    // }
 
     return (
         <div className='login-container'>
-            <form className='login-form'>
+            <form className='login-form' onSubmit={uploadToServer}>
                 <h4 className='mb-4'>Upload document</h4>
                 <input type="file" className="form-control" id="inputGroupFile02" />
-                <input className='mt-4 btn btn-info' type='button' value='Upload' />
+                <input className='mt-4 btn btn-info' type='button' value='Upload' onChange={handleFileInput} />
                 <ToastContainer />
             </form>
         </div>
